d7dbd08930aaead55bfc0f6811006f36d5dc4f72
Francisco Anacleto de Albuquerque <104237733+faalb@users.noreply.github.com>
2023-10-23 14:35:23 -0300

    Upload do gerador de senhas

---
+++ script.js
@@ -0,0 +1,32 @@
+let botaoDeslizante = document.querySelector("#botao-deslizante")
+let botaoGerarSenha = document.querySelector("#botao-gerar-senha")
+
+let tamanhoDaSenha = document.querySelector("#tamanho-senha")
+let senha = document.querySelector("#senha")
+
+let containerSenha = document.querySelector("#container-senha")
+
+let caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPGRSTUVWXYZ123456789!@_-*%&#/";
+let copiarSenha = "";
+
+tamanhoDaSenha.innerHTML = botaoDeslizante.value 
+botaoDeslizante.oninput = function() { 
+}
+
+function geradorDeSenha() {
+    let senhaGerada = ""
+    for(let i = 0, n = caracteres.length; i < botaoDeslizante.value; ++i) {
+        senhaGerada += caracteres.charAt(Math.floor(Math.random() * n))
+    }
+    containerSenha.classList.remove("esconder")
+    senha.innerHTML = senhaGerada
+    copiarSenha = senhaGerada
+}
+
+function copiarSenhaGerada() {
+    alert("Senha copiada com sucesso!") 
+    setTimeout(function() { 
+        navigator.clipboard.writeText(copiarSenha)
+    }, 100);
+}
+
